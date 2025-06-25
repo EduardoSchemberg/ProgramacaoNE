8c5cea7aa640062c740f99de9d3b4d422f9cbac6
EduardoSchemberg <eduardo.schemberg@escola.pr.gov.br>
2025-06-25 19:03:45 -0300

    js1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function inserirNome () {
-    let nomeUsuario = prompt ("Qual o seu nome?");
+    let nomeUsuario = prompt ("Qual o seu nome");
     let elemento = document.querySelector("#nome-usuário");
     elemento.textContent = nomeUsuario;
 }

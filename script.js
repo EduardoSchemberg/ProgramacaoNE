e9b590d2f2dc61bc57cac96b56d355a6c135a358
EduardoSchemberg <eduardo.schemberg@escola.pr.gov.br>
2025-07-02 18:58:48 -0300

    Listas java

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function inserirNome(){
  elemento.textContent = nomeUsuario;
 }
 inserirNome();
+
+let linguagens = ["Javascript", "Python", "C++"];
+console.log(linguagens[1])
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
